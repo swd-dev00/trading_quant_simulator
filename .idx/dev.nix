2366a35bdf9730d4484664c7f61cb8ea4c9569ce
bbnsisi <bbnsisi@sierrawarrendevelopments.com>
2026-03-12 22:12:41 +0000

    restart enviornment

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,58 +1,39 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    # pkgs.go
     pkgs.python311
     pkgs.python311Packages.pip
     pkgs.python311Packages.anthropic
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
+    pkgs.python311Packages.fastapi
+    pkgs.python311Packages.uvicorn
   ];
   # Sets environment variables in the workspace
-  env = {
-    API_KEY = "your-secret-key";
-  };
+  env = {};
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # "vscodevim.vim"
-      "google.gemini-cli-vscode-ide-companion"
+      "ms-python.python"
     ];
     # Enable previews
     previews = {
       enable = true;
       previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
+        web = {
+          command = ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "$PORT"];
+          manager = "web";
+        };
       };
     };
     # Workspace lifecycle hooks
     workspace = {
       # Runs when a workspace is first created
       onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
-        # Open editors for the following files by default, if they exist:
-        default.openFiles = [ ".idx/dev.nix" "README.md" ];
-        build = "touch build.log";
+        pip-install = "pip install -r requirements.txt";
       };
       # Runs when the workspace is (re)started
-      onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
-      };
+      onStart = {};
     };
   };
 }
